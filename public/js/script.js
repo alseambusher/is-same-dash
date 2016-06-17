f6f3c3e3ebcf3e79c8b337d8bb8a9d31df3ec102
alse <alseambusher@gmail.com>
2016-06-17 15:18:26 -0700

    adding diff

--- public/js/script.js
+++ public/js/script.js
@@ -26,16 +26,30 @@ function setCrontab(){
 		});
 	});
 }
+// This is stored in main memory of user
+// TODO destroy corresponding diff when tile is closed
+var diffs = {};
 
 // Tile
 function runTile(id){
 	var urls = getTileUrls(id);
+	var server_paths = getTileServerPaths(id);
 	$("#"+id+" .loader").removeAttr("style");
 	$.post(routes.run_tile, {urls: urls}, function(data){
+
+		diffs[id] = data.diff;
+
 		var resultTile = "<div id='"+id+"-results-same'></div>";
-		resultTile += "<div id='"+id+"-results-different'></div>";
+		resultTile += "<div id='"+id+"-diff-control'></div>";
 		$("#"+id+"-results").html(resultTile);
+
 		console.log(data);
+
+		var options = "";
+		for(var i=0; i<server_paths.length; i++){
+			options += "<option value='"+i+"'>"+server_paths[i][0]+"</option>"
+		}
+
 		var grouped = {}
 		for (var i=0; i<data.sha.length; i++){
 				if (grouped[data.sha[i]])
@@ -46,10 +60,25 @@ function runTile(id){
 		for (var sha in grouped) {
 			$("#"+id+"-results-same").append("<div class='alert alert-success' style='margin-bottom: 0px'>"+grouped[sha].join("<br/>")+"</div>");
 		}
+
+		$("#"+id+"-diff-control").append("<select class='form-control' onchange='diffTile(\""+id+"\")'>"+options+"</select>");
+		$("#"+id+"-diff-control").append("<select class='form-control' onchange='diffTile(\""+id+"\")'>"+options+"</select>");
+		$("#"+id+"-results").append("<pre class='alert alert-danger' id='"+id+"-diff-result' style='display:none'></div");
+
 		$("#"+id+" .loader").attr("style", "display:none");
 	}).error(function() {
 		$("#"+id+" .loader").attr("style", "display:none");
 	});
+}
+
+function diffTile(id){
+	var options = $("#"+id+"-diff-control select");
+	var x = parseInt(options[0].value);
+	var y = parseInt(options[1].value);
+	var total = options[0].length;
+	console.log(y*total + x);
+	$("#"+id+"-diff-result").html(diffs[id][y*total + x]);
+	$("#"+id+"-diff-result").removeAttr("style");
 }
 
 function newTile(){
@@ -112,7 +141,7 @@ function getTileServerPaths(id){
 	var server_nodes = $("#"+id+"-container .input-group-addon");
 	var result = [];
 	for(var i=0; i<path_nodes.length; i++){
-		result.push([server_nodes[i].html(), path_nodes[i].val()]);
+		result.push([server_nodes[i].innerHTML, path_nodes[i].value]);
 	}
 	return result;
 }
